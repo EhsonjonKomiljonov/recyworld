29b91978b9e53aca572196ef37d803d9e8a6bf60
EhsonjonKomiljonov <ekomiljonov29@gmail.com>
2023-10-29 16:47:58 +0500

    edit name in about card images

--- db/aboutCardData.ts
+++ db/aboutCardData.ts
@@ -1,24 +1,24 @@
 const aboutCardData: {}[] = [
   {
-    imgLink: '../src/assets/images/about-card-1.png',
+    imgLink: '../src/assets/images/about-card1.png',
     title: 'Plastic Waste Management',
     desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
     blanditiis veniam.`,
   },
   {
-    imgLink: '../src/assets/images/about-card-2.png',
+    imgLink: '../src/assets/images/about-card2.png',
     title: 'Factory Waste Management',
     desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
     blanditiis veniam.`,
   },
   {
-    imgLink: '../src/assets/images/about-card-3.png',
+    imgLink: '../src/assets/images/about-card3.png',
     title: 'e-waste Management',
     desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
     blanditiis veniam.`,
   },
   {
-    imgLink: '../src/assets/images/about-card-4.png',
+    imgLink: '../src/assets/images/about-card4.png',
     title: 'Medical Waste Management',
     desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
     blanditiis veniam.`,
